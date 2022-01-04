942cba8f7ef461c7fa86b667c1a399a6910abc8e
rafael altamirano <rafaelaltamirano_@outlook.com>
2022-01-04 20:37:11 -0300

    test

---
+++ app/src/main/java/com/walnash/navigationdrawer/MainActivity.kt
@@ -0,0 +1,95 @@
+package com.walnash.navigationdrawer
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigate
+import androidx.navigation.compose.rememberNavController
+import com.walnash.navigationdrawer.ui.theme.NavigationDrawerTheme
+import kotlinx.coroutines.launch
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NavigationDrawerTheme {
+                AppMainScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun AppMainScreen() {
+    val navController = rememberNavController()
+    Surface(color = MaterialTheme.colors.background) {
+        val drawerState = rememberDrawerState(DrawerValue.Closed)
+        val scope = rememberCoroutineScope()
+        val openDrawer = {
+            scope.launch {
+                drawerState.open()
+            }
+        }
+        CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Rtl)  {
+            ModalDrawer(
+                drawerState = drawerState,
+                gesturesEnabled = drawerState.isOpen,
+                drawerContent = {
+                    ModalDrawerTest(
+                        onDestinationClicked = { route ->
+                            scope.launch {
+                                drawerState.close()
+                            }
+                            navController.navigate(route) {
+                                popUpTo = navController.graph.startDestination
+                                launchSingleTop = true
+                            }
+                        }
+                    )
+                }
+            ) {
+                NavHost(navController = navController,
+                    startDestination = DrawerScreens.Home.route
+                ) {
+                    composable(DrawerScreens.Home.route) {
+                        Home(
+                            openDrawer = {
+                                openDrawer()
+                            }
+                        )
+                    }
+                    composable(DrawerScreens.Account.route) {
+                        Account(
+                            openDrawer = {
+                                openDrawer()
+                            }
+                        )
+                    }
+                    composable(DrawerScreens.Help.route) {
+                        Help(
+                            navController = navController
+                        )
+                    }
+                }
+            }
+            }
+        }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    NavigationDrawerTheme {
+        AppMainScreen()
+    }
+}
---
+++ app/src/main/java/com/walnash/navigationdrawer/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.walnash.navigationdrawer.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+val Primary = Color(0xFF6C8AE8)
